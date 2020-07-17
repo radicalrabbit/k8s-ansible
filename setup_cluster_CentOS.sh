1a455530ab012f4223a2d5c41969d4ff0585ad05
alex <alex@radicalrabbit.de>
2020-07-17 23:02:40 +0200

    Removed flannel pod network crash issue with the dashboard.

--- setup_cluster_CentOS.sh
+++ setup_cluster_CentOS.sh
@@ -3,7 +3,11 @@ echo "### Installing Kubernetes Cluster (CentOS)."
 echo "Preparing Vagrant Boxes (CentOS VMs)..."
 cd vagrant
 sudo vagrant destroy
+sudo ssh-keygen -R 10.11.12.2
+sudo ssh-keygen -R 10.11.12.3
+sudo ssh-keygen -R 10.11.12.4
 sudo vagrant up
+sleep 5
 cd ..
 echo "Installing Kubernetes Nodes (1 Master, 2 Workers)..."
 cd centos
