2799b12da3481966025b4a8bcf8f3792b8e43d0a
alex <alex@radicalrabbit.de>
2020-07-23 22:25:23 +0200

    Moved pod deployment yml to extra folder.

--- setup_cluster_CentOS.sh
+++ setup_cluster_CentOS.sh
@@ -12,9 +12,13 @@ cd ..
 echo "Installing Kubernetes Master-Node..."
 cd centos
 sudo ansible-playbook setup_master_node.yml
+cd ..
+cd deployment
 echo "Installing Kubernetes Dashboard..."
 sudo ansible-playbook setup_dashboard.yml
 sleep 10
+cd ..
+cd centos
 echo "Installing Kubernetes Worker Nodes (2 Nodes)..."
 sudo ansible-playbook setup_worker_nodes.yml
 
